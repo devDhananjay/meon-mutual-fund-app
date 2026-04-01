f51d1549e0db575575a3442c58f20028d279434f
devDhananjay <dhananjay@meon.co.in>
2026-04-01 13:22:22 +0530

    updatedcode

--- android/gradlew.bat
+++ android/gradlew.bat
@@ -35,9 +35,6 @@ set APP_HOME=%DIRNAME%
 @rem Resolve any "." and ".." in APP_HOME to make it shorter.
 for %%i in ("%APP_HOME%") do set APP_HOME=%%~fi
 
-@rem Keep Gradle cache local to the project to avoid broken global cache state.
-set GRADLE_USER_HOME=%APP_HOME%\.gradle-user-home
-
 @rem Add default JVM options here. You can also use JAVA_OPTS and GRADLE_OPTS to pass JVM options to this script.
 set DEFAULT_JVM_OPTS="-Xmx64m" "-Xms64m"
 
